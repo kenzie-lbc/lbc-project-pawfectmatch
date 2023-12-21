bb3a14a3cd01e76352c13e3cc3253f4c6b22cff7
jvubeepboop <john.vu@mykenzie.snhu.edu>
2023-12-20 17:14:21 -0800

    Updated Pet & User model/record with enum values for each

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/enums/Role.java
@@ -0,0 +1,7 @@
+package com.kenzie.appserver.repositories.enums;
+
+public enum Role {
+    SHELTER,
+    ADOPTER,
+    FOSTER;
+}
--- Application/src/main/java/com/kenzie/appserver/service/PetService.java
+++ Application/src/main/java/com/kenzie/appserver/service/PetService.java
@@ -1,10 +1,15 @@
 package com.kenzie.appserver.service;
 
 import com.kenzie.appserver.repositories.PetRepository;
+import com.kenzie.appserver.repositories.enums.PetType;
 import com.kenzie.appserver.repositories.model.Pet;
+import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.kenzie.appserver.repositories.enums.PetType.CAT;
+import static com.kenzie.appserver.repositories.enums.PetType.DOG;
 
 @Service
 public class PetService {
@@ -17,36 +22,46 @@ public class PetService {
     }
 
     // Method to handle saving a new pet
-    public Pet createPet(Pet pet) {
+    public Pet createPet(Pet pet) throws InvalidPetException {
+        if (StringUtils.isEmpty(pet.getName())) {
+            throw new InvalidPetException("Pet name is required");
+        }
+        if (pet.getAge() <= 0) {
+            throw new InvalidPetException("Pet age must be greater than 0");
+        }
+// Validate other fields...
         // Set PetID using UniqueIdGenerator
-        pet.setId(UniqueIdGenerator.generatePetId(pet.getType()));
+//        pet.setId(UniqueIdGenerator.generatePetId(pet.getType()),
 
-        // Get the userId of the user creating the pet
+//        // Set image
+//        pet.setImageUrl(imageUrl);
+//
+//        // Get the userId of the user creating the pet
 //        String userId = getLoggedInUserId();
-
-        // Set the adoptionId to the userId
+//
+//        // Set the adoptionId to the userId
 //        pet.setAdoptionId(userId);
-        // Save pet using repository
-        petRepository.save(pet);
 
+        // Save pet using repository
+//        petRepository.save(pet).toString())
         // Return saved pet
-        return pet;
+        ;return pet;
     }
 
     // Method to find pets by type
-    public List<Pet> findPetsByType(String type) {
-        return petRepository.findByType(type);
+    public List<Pet> findPetsByType(PetType petType) {
+        return petRepository.findByType(petType);
     }
 
     // Other methods specific to certain pet types
 
     // Method to find dogs
     public List<Pet> findDogs() {
-        return petRepository.findByType("Dog");
+        return petRepository.findByType(DOG);
     }
 
     // Method to find cats
     public List<Pet> findCats() {
-        return petRepository.findByType("Cat");
+        return petRepository.findByType(CAT);
     }
 }
